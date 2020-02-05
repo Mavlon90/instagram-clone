5dd1862cdf12b2642ce0494f5ebd2ebfebee8a7d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-05 16:40:15 +0000

    Deploy Mavlon90/instagram-clone to github.com/Mavlon90/instagram-clone.git:gh-pages

---
+++ precache-manifest.dfe177de6bc919b340718be39ff98a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2052f13120528f252d37608b03617ccd",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "d46431587d301143c8f2",
+    "url": "/instagram-clone/static/css/main.ab3633df.chunk.css"
+  },
+  {
+    "revision": "651bb73b4f38fea23741",
+    "url": "/instagram-clone/static/js/2.5e602cee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/instagram-clone/static/js/2.5e602cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46431587d301143c8f2",
+    "url": "/instagram-clone/static/js/main.ddb2263c.chunk.js"
+  },
+  {
+    "revision": "f17c327e2e2ccaee0e6a",
+    "url": "/instagram-clone/static/js/runtime-main.90408c0c.js"
+  },
+  {
+    "revision": "65ffa5ba8be30c2385827338d50a227b",
+    "url": "/instagram-clone/static/media/sprite.65ffa5ba.png"
+  }
+]);
